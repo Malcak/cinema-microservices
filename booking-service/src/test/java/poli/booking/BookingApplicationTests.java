d1a211ae1fc9d61e51e814e316126ef915dbfad3
Malcak <malcakx@gmail.com>
2021-09-09 21:46:35 -0500

    create booking microservice

---
+++ booking-service/src/test/java/poli/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package poli.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
