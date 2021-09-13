e7d7e8df33fc721387bffbe8ee9a34e97afb1d38
Malcak <malcakx@gmail.com>
2021-09-12 21:09:29 -0500

    add gateway and admin service

--- booking-service/src/main/java/poli/booking/clients/UserClient.java
+++ booking-service/src/main/java/poli/booking/clients/UserClient.java
@@ -3,7 +3,6 @@ package poli.booking.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import poli.booking.models.User;
 
 @FeignClient(name = "user-service", fallback = UserClientFallBackHystrix.class)
